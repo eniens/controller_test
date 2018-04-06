539b6a3712af4f9249db7fb2d1e60a2a2927c2a1
Eric R Niens <eniens@gmail.com>
2018-04-06 00:52:28 -0300

    Alterações no modulo para utilizar um HTML simples

--- controller_module/controllers/controllers.py
+++ controller_module/controllers/controllers.py
@@ -1,10 +1,109 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
+import os
+import jinja2
+
+from os import walk
+import pathlib
+
+loader = jinja2.FileSystemLoader('/mnt/extra-addons/controller_module/web/views')
+env = jinja2.Environment(loader=loader, autoescape=True)
+# env = jinja2.Environment(loader=loader, autoescape=True)
 
 class ControllerModule(http.Controller):
+    def _render_template(self, **d):
+        print("------------------------------------------------------------------------")
+        print(os.getcwd())
+        d.setdefault('manage', True)
+        d['regioes'] = [['R1','Ibirapuera'], ['R2','Mooca'], ('R3','Ana Rosa')]
+        # d.setdefault('manage',True)
+        # d['insecure'] = odoo.tools.config.verify_admin_password('admin')
+        d['list_db'] = ['DB1','DB2']
+        # d['langs'] = odoo.service.db.exp_list_lang()
+        d['especs'] = [
+['acupuntura','acupuntura'],
+['alergologista','alergia e imunologia'],
+['alergologista_pediatrico','alergia e imunologia pediatrica'],
+['cardiologista','cardiologia'],
+['cardiologistacongenita','cardiologia congenita'],
+['cardiologiainfantil','cardiologia pediatrica'],
+['cirurgia-aparelho-digestivo','cirurgia do aparelho digestivo'],
+['cirurgiao-geral','cirurgia geral'],
+['cirurgiapediatrica','cirurgia pediatrica'],
+['cirurgiao-plastico','cirurgia plastica'],
+['cirurgiao-vascular','cirurgia vascular'],
+['clinico-geral','clinica geral'],
+['coloproctologista','coloproctologia'],
+['dermatologista','dermatologia'],
+['endocrinologista','endocrinologia'],
+['endocrinologiainfantil','endocrinologia pediatrica'],
+['fisioterapeuta','fisioterapia'],
+['gastroenterologista','gastroenterologia'],
+['gastroenterologiainfantil','gastroenterologia pediatrica'],
+['geriatra','geriatria'],
+['ginecologista','ginecologia e obstetricia'],
+['hematologista','hematologia'],
+['HEMATOLOGISTA_PEDIATRICO','hematologia pediatrica'],
+['hepatologista','hepatologia'],
+['homeopatia','homeopatia'],
+['infectologista','infectologia'],
+['mastologista','mastologia'],
+['medicina_esportiva','medicina esportiva'],
+['nefrologista','nefrologia'],
+['nefrologiainfantil','nefrologia pediatrica'],
+['neurologista','neurologia'],
+['neurologiainfantil','neurologia pediatrica'],
+['nutricionista','nutricao'],
+['nutrologia','nutrologia'],
+['dentista','odontologia (avaliacao odontologica)'],
+['oftalmologista','oftalmologia'],
+['oftalmologista_ped','oftalmologia pediatrica'],
+['ortopedista','ortopedia'],
+['otorrinolaringologista','otorrinolaringologia'],
+['pediatra','pediatria'],
+['pneumologista','pneumologia'],
+['pneumoinfantil','pneumologia pediatrica'],
+['psicologo','psicologia'],
+['psiquiatra','psiquiatria'],
+['psiquatriainfantil','psiquiatria pediatrica'],
+['reumatologista','reumatologia'],
+['reumatologista_pedriatrico','reumatologia pediatrica'],
+['urologista','urologia']
+]
+        # d['countries'] = odoo.service.db.exp_list_countries()
+        # d['pattern'] = DBNAME_PATTERN
+        # # databases list
+        # d['databases'] = []
+        # try:
+        #     d['databases'] = http.db_list()
+        #     d['incompatible_databases'] = odoo.service.db.list_db_incompatible(d['databases'])
+        # except odoo.exceptions.AccessDenied:
+        #     monodb = db_monodb()
+        #     if monodb:
+        #         d['databases'] = [monodb]
+        return env.get_template("teste.html").render(d)
+
+    @http.route('/dbselector', type='http', auth="none")
+    def selector(self, **kw):
+        print("------------------------------------------------------------------------")
+        current_dir = pathlib.Path(__file__).parent
+        print(current_dir)
+        print("------------------------------------------------------------------------")
+        request._cr = None
+        return self._render_template(manage=False)
+
     @http.route('/empresas/', auth='none')
     def index(self, **kw):
+        print("------------------------------------------------------------------------")
+        print(os.getcwd())
+        # print("Caminho:" & os.path.dirname(__file__))
+        # loader = jinja2.PackageLoader('YourCustomAddonPath', "yourcustomhtmlfilepathresidingfolder eg:views")
+        # loader = jinja2.PackageLoader('odoo.addons.web', "views")
+        # env.get_template("custom_html.html").render(d).
+
+
+
         page = open('/odoo11/custom/controller_module/static/src/html/index.html').read()
         partners = request.env['res.partner'].sudo().search([('customer','=',True)])
         for partner in partners:
@@ -41,4 +140,3 @@ class ControllerModule(http.Controller):
         if post:
 
             print(post)
-
